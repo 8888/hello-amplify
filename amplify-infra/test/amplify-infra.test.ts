15d021cad15dfdb5ffdd117023384ad23560a171
8888 <leecostello1@gmail.com>
2021-12-18 10:58:49 -0500

    setup infra w/ cdk v1 for amplify

---
+++ amplify-infra/test/amplify-infra.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as AmplifyInfra from '../lib/amplify-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/amplify-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AmplifyInfra.AmplifyInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
