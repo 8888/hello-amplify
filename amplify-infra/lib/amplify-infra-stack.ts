9c31da277359a1db1448eacc0c7e6b974e95f8f3
8888 <leecostello1@gmail.com>
2021-12-18 11:07:26 -0500

    create new amplify app

--- amplify-infra/lib/amplify-infra-stack.ts
+++ amplify-infra/lib/amplify-infra-stack.ts
@@ -1,15 +1,18 @@
 import * as cdk from '@aws-cdk/core';
-// import * as sqs from '@aws-cdk/aws-sqs';
+import * as amplify from "@aws-cdk/aws-amplify";
 
 export class AmplifyInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const amplifyApp = new amplify.App(this, 'hello-amplify', {
+      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
+        owner: '8888',
+        repository: 'hello-amplify',
+        oauthToken: cdk.SecretValue.secretsManager('gh/pat/hello-amplify', { jsonField: 'pat' }),
+      })
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AmplifyInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const masterBranch = amplifyApp.addBranch('main');
   }
 }
